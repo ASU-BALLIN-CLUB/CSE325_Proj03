2ba11a6aff231e0dd5ccf75792ca0853833187d9
Slamkas <seslamka@yahoo.com>
2014-10-08 15:03:14 -0700

    Update uc_pushb.c

--- uc_pushb.c
+++ uc_pushb.c
@@ -17,7 +17,7 @@ void uc_pushb_init(uc_pushb_t const p_button, int_isr_t const p_callback)
 	g_dtim_callbacks[p_button] = p_callback;
 	gpio_port_init(gpio_port_ta, gpio_pin_0, gpio_funct_primary, gpio_data_dir_x, gpio_state_x);
 	gpt_disable();
-	gpt_incap_config(gpio_pin_1, gpt_incap_edge_t const p_incap_edge)
+	gpt_incap_config(gpt_pin_0, gpt_incap_edge_falling)
 	if( p_button == uc_pushb_1)
 	{
 		int_config(gpt0_int_src, gpt0_int_level, gpt0_int_priority, uc_pushb_1_isr)
