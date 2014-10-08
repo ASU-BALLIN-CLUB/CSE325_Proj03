6bec34f1b88e59b038a1ebfcc99671bfcd4a4af9
Slamkas <seslamka@yahoo.com>
2014-10-08 15:04:50 -0700

    Update us_pushb.h

--- us_pushb.h
+++ us_pushb.h
@@ -7,3 +7,18 @@
 //  CSE325 Embedded Microprocessor Systems
 //  Fall 2014
 //
+#ifndef ____uc_pushb__
+#define ____uc_pushb__
+#include "support_common.h"
+
+
+typedef enum {
+ uc_pushb_1 = 0, // Represents
+ uc_pushb_2 = 1, // Represents
+} uc_pushb_t;
+
+void uc_pushb_init(uc_pushb_t const p_button, int_isr_t const p_callback);
+__declspec(interrupt) void uc_pushb_1_isr();
+__declspec(interrupt) void uc_pushb_2_isr();
+
+#endif
