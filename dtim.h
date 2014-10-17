9354862156b6160ae19eac5fb97aa6b4bbb9ef8b
abalci <abalci@asu.edu>
2014-10-16 18:03:42 -0700

    Update dtim.h

--- dtim.h
+++ dtim.h
@@ -1,7 +1,7 @@
 //
 //  dtim.h
 //
-//	Proj02
+//	Proj03
 //  Lab Partners: Sean Slamka, Aydin Balci
 //  Email: sslamka@asu.edu, abalci@asu.edu
 //  CSE325 Embedded Microprocessor Systems
@@ -12,6 +12,9 @@
 
 #include "gpio.h"
 #include "support_common.h"
+#include "int.h"
+#include "gpt.h"
+#include "uc_pushb.h"
 
 //Header file for dtim function
 typedef enum {
@@ -21,9 +24,17 @@ typedef enum {
  dtim_3 = 3 // DMA timer 3
 } dtim_t;
 
+void dtim_init_irq(dtim_t const p_timer, uint32 const p_usecs, int_isr_t const p_callback);
 void dtim_busy_delay_ms(dtim_t const p_timer, uint32 const p_msecs);
 void dtim_busy_delay_us(dtim_t const p_timer, uint32 const p_usecs);
 void dtim_init(dtim_t const p_timer);
+__declspec(interrupt) void dtim0_isr();
+__declspec(interrupt) void dtim1_isr();
+__declspec(interrupt) void dtim2_isr();
+__declspec(interrupt) void dtim3_isr();
+static int_isr_t g_dtim_callbacks[4] = {0};
+static int_isr_t g_dtim_irsrs[4] = {dtim0_isr, dtim1_isr, dtim2_isr, dtim3_isr};
+
 //static uint32 const DTIM_BASE = 0x40000400;
 
 #endif /*  defined (__dtim__) */
